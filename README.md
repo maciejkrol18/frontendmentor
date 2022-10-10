44aeda082ced9cbd8314bd8f995cb347b8b2072e
Fancy <maciekgplay@gmail.com>
2022-10-10 20:51:17 +0200

    ✨New project
    * README.md update
    * New project - tip calculator app. Functionality done. TODO: styling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 🎓 Frontendmentor
 This repository contains all of the **Frontendmentor** challanges i've completed so far.\
-Live sites can be easily accessed from my github.io homepage at <a href="https://fancybaguette.github.io/#challanges">fancybaguette.github.io</a>
+Live sites can be easily accessed from my github.io homepage at <a href="https://fancybaguette.github.io/#challanges">fancybaguette.github.io</a>\
+Or simply by going to fancybaguette.github.io/frontendmentor/FOLDER_NAME_HERE
---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,59 @@
+// DOM references
+
+// * Inputs
+const billAmountInput = document.querySelector('.bill-amount-input');
+const numberOfPeopleInput = document.querySelector('.number-of-people-input');
+const customPercentageInput = document.querySelector('.custom-percentage-input');
+
+// * Result elements
+const tipPerPersonResultEl = document.querySelector('.tip-per-person-result');
+const tipTotalResultEl = document.querySelector('.tip-total-result');
+
+function getCheckedTipPercentage() {
+    if (customPercentageInput.value === "" || customPercentageInput.value === undefined || customPercentageInput.value === null) {
+        return document.querySelector('.tip-percentage-input:checked').dataset['percentage'];
+    } else {
+        return Number((customPercentageInput.value) / 100);
+    }
+}
+
+billAmountInput.addEventListener('keydown', renderResult);
+numberOfPeopleInput.addEventListener('keydown', renderResult);
+customPercentageInput.addEventListener('keydown', renderResult);
+customPercentageInput.addEventListener('keydown', resetPercentageLabelsColor);
+
+document.querySelectorAll('.tip-percentage-input').forEach((e) => {
+    e.addEventListener('change', () => {
+        customPercentageInput.value = "";
+        renderResult();
+    });
+})
+
+const percentageLabels = document.querySelectorAll('.tip-percentage-label');
+
+percentageLabels.forEach((label) => {
+    label.addEventListener('click', (e) => {
+        resetPercentageLabelsColor();
+        e.target.style.backgroundColor = "var(--clr-strong-cyan)"
+    });
+})
+
+function resetPercentageLabelsColor() {
+    percentageLabels.forEach((label) => {
+        label.style.backgroundColor = "var(--clr-very-dark-cyan)"
+    })
+}
+
+function renderResult() {
+
+    const billAmount = Number(billAmountInput.value);
+    const tipPercentage = Number(getCheckedTipPercentage());
+    const numberOfPeople = Number(numberOfPeopleInput.value);
+
+    const tipPerPerson = Number(((billAmount * tipPercentage) / numberOfPeople).toFixed(2));
+    const tipTotal = Number(((billAmount / numberOfPeople) + tipPerPerson).toFixed(2));
+
+    tipPerPersonResultEl.textContent = tipPerPerson;
+    tipTotalResultEl.textContent = tipTotal;
+
+}
