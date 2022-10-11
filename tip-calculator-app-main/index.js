f44d95705279fcfa8b03315a82b5db7046377303
Fancy <maciekgplay@gmail.com>
2022-10-11 19:18:21 +0200

    asdasd

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -4,22 +4,25 @@
 const billAmountInput = document.querySelector('.bill-amount-input');
 const numberOfPeopleInput = document.querySelector('.number-of-people-input');
 const customPercentageInput = document.querySelector('.custom-percentage-input');
+const resetBtn = document.querySelector('.reset-btn')
 
 // * Result elements
 const tipPerPersonResultEl = document.querySelector('.tip-per-person-result');
 const tipTotalResultEl = document.querySelector('.tip-total-result');
 
 function getCheckedTipPercentage() {
-    if (customPercentageInput.value === "" || customPercentageInput.value === undefined || customPercentageInput.value === null) {
+    if (customPercentageInput.value === "") {
         return document.querySelector('.tip-percentage-input:checked').dataset['percentage'];
     } else {
         return Number((customPercentageInput.value) / 100);
     }
 }
 
-billAmountInput.addEventListener('keydown', renderResult);
-numberOfPeopleInput.addEventListener('keydown', renderResult);
-customPercentageInput.addEventListener('keydown', renderResult);
+document.addEventListener('keydown', renderResult)
+
+// billAmountInput.addEventListener('keydown', renderResult);
+// numberOfPeopleInput.addEventListener('keydown', checkIfNumberOfPeopleInvalid);
+// customPercentageInput.addEventListener('keydown', renderResult);
 customPercentageInput.addEventListener('keydown', resetPercentageLabelsColor);
 
 document.querySelectorAll('.tip-percentage-input').forEach((e) => {
@@ -44,21 +47,45 @@ function resetPercentageLabelsColor() {
     })
 }
 
+// function checkIfNumberOfPeopleInvalid() {
+//     if (numberOfPeopleInput.value === 0 || numberOfPeopleInput.value === "") {
+//         numberOfPeopleInput.style.outline = "1px solid red"
+//         console.log('invalid')
+//     } else {
+//         numberOfPeopleInput.style.outline = "5px solid var(--clr-strong-cyan)"
+//     }
+// }
+
 function renderResult() {
+
+    document.querySelector('.tip-percentage-input:checked').style.backgroundColor = "var(--clr-strong-cyan) !important"
 
     const billAmount = Number(billAmountInput.value);
     const tipPercentage = Number(getCheckedTipPercentage());
     const numberOfPeople = Number(numberOfPeopleInput.value);
 
+    if (billAmount === 0) {
+        billAmountInput.dataset.invalid = true;
+    } else {
+        billAmountInput.dataset.invalid = false;
+    }
+
     const tipPerPerson = Number(((billAmount * tipPercentage) / numberOfPeople).toFixed(2));
     const tipTotal = Number(((billAmount / numberOfPeople) + tipPerPerson).toFixed(2));
 
-    if (tipPerPerson === NaN || tipPerPerson === Infinity || tipTotal === NaN || tipTotal === Infinity) {
+    if (
+        billAmount === 0 || tipPercentage === 0 || numberOfPeople === 0
+        ) {
         tipPerPersonResultEl.textContent = `...`;
         tipTotalResultEl.textContent = `...`;
     } else {
-        tipPerPersonResultEl.textContent = tipPerPerson;
-        tipTotalResultEl.textContent = tipTotal;
+        tipPerPersonResultEl.textContent = `$${tipPerPerson}`;
+        tipTotalResultEl.textContent = `$${tipTotal}`;
     }
-
 }
+
+resetBtn.addEventListener('click', () => {
+    billAmountInput.value = 0;
+    numberOfPeopleInput.value = 0;
+    renderResult();
+})
